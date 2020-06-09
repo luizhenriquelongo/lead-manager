8deec0f1475e7c3eaebd1d7c19ee244139dc7daa
luizhenriquelongo <longo.luizh@gmail.com>
2020-06-08 23:38:21 -0300

    adding token authentication for urls

---
+++ leadmanager/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from knox.views import LogoutView
+
+from .api import RegisterAPI, LoginAPI, UserAPI
+
+
+urlpatterns = [
+    path('api/auth', include('knox.urls')),
+    path('api/auth/register', RegisterAPI.as_view()),
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/logout', LogoutView.as_view(), name='knox_logout'),
+]
--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -39,10 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'knox',
 
     'leads',
+    'accounts',
     'frontend',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
